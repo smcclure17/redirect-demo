ae5af4bc364a92bbad20df04a7d85877ef278610
Sean McClure <sean.mcclure17@gmail.com>
2022-09-20 21:48:08 -0400

    Add registerUrl request, lots of TODOs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,31 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { assert } from "@actnowcoalition/assert";
+import { UrlData } from "./types";
+
 admin.initializeApp();
 
 /**
  * Generates a unique shortened url slug for a url document whenever one is created.
  *
- * This slug is appended to the document as the field "shortUrl".
+ * This slug is appended to the document as the field "shortUrl". 
+ * If the document already has a "shortUrl" field, this function does nothing.
+ * 
+ * See TODO below. This ultimately should be replaced, but it's handy for the moment.
  */
 exports.createShortUrl = functions.firestore
   .document("/urls/{documentId}")
   .onCreate(async (snap) => {
     const recordData = snap.data();
     const random = (Math.random() + 1).toString(36).substring(2);
-
-    return snap.ref.set({
-      ...recordData,
-      shortUrl: random,
-    });
+    if (recordData.shortUrl) {
+      return snap
+    } else {
+      return snap.ref.set({
+        ...recordData,
+        shortUrl: random,
+      });
+    }
   });
 
 /**
@@ -35,7 +44,7 @@ exports.url = functions.https.onRequest((req, res) => {
       .status(400)
       .send(
         "Missing URL parameter." +
-          "\n Expected structure: https://....net/url/SHORT_URL_HERE"
+        "\n Expected structure: https://....net/url/SHORT_URL_HERE"
       );
     return;
   }
@@ -63,6 +72,42 @@ exports.url = functions.https.onRequest((req, res) => {
       }
     });
 });
+
+/**
+ * Register a new shortened url.
+ * 
+ * Requires `content-type: application/json` header and a JSON body (document the args here).
+ */
+exports.registerUrl = functions.https.onRequest((req, res) => {
+  const imageUrl = req.body.imageUrl as string;
+  const imageScreenshotUrl = req.body.imageScreenshotUrl as string;
+  assert(imageUrl || imageScreenshotUrl, "imageUrl or imageScreenshotUrl must be provided");
+  // TODO: We should probably make the shortUrl the ID of the document because:
+  // 1. The current document ID is currently never used/is useless.
+  // 2. The shortUrl should be unique
+  // 3. Then we could return the shortUrl in the response here, allowing the client to use it without any more requests.
+  // So, we should move the shortUrl generation to here, and remove the document modifying function.
+  
+  // For now, I'm just duplicating the logic from createShortUrl so that we can return the shortUrl in the response
+  // without a race condition.
+  const shortUrl = (Math.random() + 1).toString(36).substring(2);
+  const data: UrlData = {
+    imageUrl: imageUrl ?? "",
+    imageScreenshotUrl: imageScreenshotUrl ?? "",
+    url: req.body.url ?? "",
+    title: req.body.title ?? "",
+    description: req.body.description ?? "",
+    shortUrl: shortUrl,
+  }
+
+  const db = admin.firestore();
+  db.collection("urls").doc().set(data).then(() => {
+    const baseUrl = "https://us-central1-test-url-api.cloudfunctions.net/url/";
+    res.status(200).send(`${baseUrl}${shortUrl}`);
+  }).catch((err) => {
+    res.status(500).send(`error ${JSON.stringify(err)}`);
+  })
+})
 
 /**
  * Fetch corresponding data for a given shortened url from Firestore.
---
+++ functions/src/types.ts
@@ -0,0 +1,8 @@
+export type UrlData = {
+    url: string;
+    imageUrl: string;
+    imageScreenshotUrl?: string;
+    title: string;
+    description: string;
+    shortUrl?: string;
+}
